6a6e4fbd4a53220f5e5972ec3c876054ead2af7a
dew02 <syhan0108@naver.com>
2022-03-02 10:01:46 +0900

    Q10718

---
+++ 한이슬/Q10718.py
@@ -0,0 +1,6 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("강한친구 대한육군 \n강한친구 대한육군")
+강한친구 대한육군 
+강한친구 대한육군
+>>> 
