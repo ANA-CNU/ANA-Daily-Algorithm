3f3478a42278495fec55e732bd49d13b3598a559
rladbrua0207 <rladbrua0207@gmail.com>
2022-03-03 23:13:18 +0900

    Q11399

---
+++ 김유겸/Q11399.java
@@ -0,0 +1,31 @@
+package _2022_1학기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q11399 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= i; j++) {
+                sum += arr[j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
