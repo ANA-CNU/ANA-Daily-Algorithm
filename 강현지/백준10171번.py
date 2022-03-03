842580ccc5efd5b32323582bd1f70ddee056ded4
hyeond401 <100653281+hyeond401@users.noreply.github.com>
2022-03-03 09:59:54 +0900

    Add files via upload

---
+++ 강현지/백준10171번.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  3 09:29:31 2022
+
+@author: guswl
+"""
+#첫번째 방법
+print("\\    /\\ \n )  ( \') \n(  /  ) \n \\(__)|")
+
+#두번째 방법
+print("\\    /\\")
+print(" )  ( \') ")
+print("(  /  ) ")       
+print(" \\(__)|")
