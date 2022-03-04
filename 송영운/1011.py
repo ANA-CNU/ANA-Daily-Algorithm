59488ff8ca6abb02399aa233f9ea4d97acb661f3
songyoungwoon <thdduddns98@naver.com>
2022-03-04 17:19:15 +0900

    백준 1011

---
+++ 송영운/1011.py
@@ -0,0 +1,24 @@
+import math
+from collections import deque
+input = __import__('sys').stdin.readline
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        q = deque()
+
+        x, y = map(int, input().split())
+        d = y - x
+        d_sqrt = int(math.sqrt(d)) + 1
+        d_sqrt_pow = d_sqrt**2
+        d_sqrt_count = 2*d_sqrt - 1
+        minus_count = d_sqrt_pow - d
+        count = 0
+        for i in range(minus_count):
+            count += 1
+            if count == d_sqrt:
+                d_sqrt_count -= 1
+                d_sqrt -= 1
+                count = 0
+
+        print(d_sqrt_count)
+
