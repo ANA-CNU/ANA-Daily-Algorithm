51827423f5290dd40e74ec3acd6b79b6faca2972
hyeond401 <100653281+hyeond401@users.noreply.github.com>
2022-03-04 10:31:35 +0900

    백준10172번

---
+++ 강현지/백준10172번.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  4 10:22:33 2022
+
+@author: guswl
+"""
+
+print("|\\_/|")
+print("|q p|   /}")
+print('( 0 )"""\\')
+print('|"^"`    |')
+print('||_/=\\\\__|')
+
+
+print('|\\_/|\n|q p|   /}\n( 0 )"""\\\n|"^"`    |\n||_/=\\\\__|')
