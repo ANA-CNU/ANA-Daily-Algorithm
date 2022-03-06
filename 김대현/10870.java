8a53bf80783de5366f15cf8e84ad6ee137fef8c5
rladh06 <33508543+rladh06@users.noreply.github.com>
2022-03-06 19:10:18 +0900

    Update and rename Main.java to 10870.java

--- 김대현/10870.java
+++ 김대현/10870.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class Main {
+public class 10870 {
 	
 	public static void main(String[] args) throws IOException{
 		
