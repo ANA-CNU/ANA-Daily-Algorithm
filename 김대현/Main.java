d493fba970c098863b32b8eb9accb423296ac40d
rladh06 <33508543+rladh06@users.noreply.github.com>
2022-03-06 19:08:21 +0900

    Add files via upload

---
+++ 김대현/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		
+		Main m = new Main();
+		
+		System.out.println(m.Fibonacci(a));
+		}
+	
+	public int Fibonacci(int n) {
+		if(n == 0) return 0;
+		if(n == 1) return 1;
+		return Fibonacci(n-1) + Fibonacci(n-2);
+	}
+	}
+	
+	
+
+
+
