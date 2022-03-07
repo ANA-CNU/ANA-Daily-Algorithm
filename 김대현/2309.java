0e9eb31657967171fc0654b265ce400748a88240
rladh06 <33508543+rladh06@users.noreply.github.com>
2022-03-07 23:56:01 +0900

    Update and rename Main.java to 2309.java

--- 김대현/2309.java
+++ 김대현/2309.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 
-public class Main {	
+public class 2309 {	
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
