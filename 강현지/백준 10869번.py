846866a6ac00466654cb67a53699f773613b5af3
hyeond401 <100653281+hyeond401@users.noreply.github.com>
2022-03-08 15:01:29 +0900

    백준 10869번

---
+++ 강현지/백준 10869번.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 14:48:33 2022
+
+@author: guswl
+"""
+
+A, B = map(int, input().split())
+
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
+
+print(A+B, A-B, A*B,A//B, A%B, sep = "\n")
