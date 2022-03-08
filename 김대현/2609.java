a5bb383e2d7923adf38a0166d3b556d8ca966b1c
rladh06 <33508543+rladh06@users.noreply.github.com>
2022-03-08 20:32:08 +0900

    Update and rename Main.java to 2609.java

--- 김대현/2609.java
+++ 김대현/2609.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 import java.util.Scanner;
 
 
-public class Main {	
+public class 2609 {	
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
