85372dd1fd45a3cf8c5d10e7d86951a056d68384
dew02 <syhan0108@naver.com>
2022-03-11 10:04:49 +0900

    Q.2588

---
+++ 한이슬/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = 0;
+		int b = 0;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a * (b % 10));
+		System.out.println(a * ((b % 100) / 10));
+		System.out.println(a * (b / 100));
+		System.out.println(a * b);
+	}
+
+}
