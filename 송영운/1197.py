c4a5799d972aafb59927cb3cc8e1171b2fda9192
songyoungwoon <thdduddns98@naver.com>
2022-03-12 00:48:25 +0900

    boj 1197 heapq

--- 송영운/1197.py
+++ 송영운/1197.py
@@ -3,20 +3,25 @@ def find(n):
         n = root_node[n]
     return n
 
-from queue import PriorityQueue
+#from queue import PriorityQueue
+from heapq import heappush, heappop
 input = __import__('sys').stdin.readline
 if __name__ == "__main__":
     v, e = map(int, input().split())
     root_node = [i for i in range(v+1)]
 
-    pq = PriorityQueue()
+    #pq = PriorityQueue()
+    pq = []
     min_price = 0
     for _ in range(e):
         a, b, c = map(int, input().split())
-        pq.put((c, a, b))
+        # pq.put((c, a, b))
+        heappush(pq, (c,a,b))
 
-    while not pq.empty():
-        c, a, b = pq.get()
+    #while not pq.empty():
+    while pq:
+        #c, a, b = pq.get()
+        c, a, b = heappop(pq)
         root_a = find(a)
         root_b = find(b)
         if root_a != root_b:
