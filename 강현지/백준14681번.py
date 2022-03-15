030e514ac3112eced21cb77aa1ed0c84c48e17ec
hyeond401 <100653281+hyeond401@users.noreply.github.com>
2022-03-15 11:14:32 +0900

    20220315

---
+++ 강현지/백준14681번.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 11:09:10 2022
+
+@author: guswl
+"""
+
+x = int(input())
+y = int(input())
+
+if x > 0:
+    if y > 0:
+        print(1)
+    else:
+        print(4)
+else:
+    if y > 0:
+        print(2)
+    else:
+        print(3)
+
+    
+    
