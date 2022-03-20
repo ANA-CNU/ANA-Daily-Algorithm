ec3d482e3b3f033f6ecd974fbb5cc2060f8d46fd
dew02 <syhan0108@naver.com>
2022-03-20 19:55:44 +0900

    Q10950

---
+++ 한이슬/Q_10950.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Q_10950 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a, b, k;
+		k = sc.nextInt();
+		for (int i = 1; i <= k; i++) {
+			a=sc.nextInt();
+			b=sc.nextInt();
+			System.out.println(a+b);
+		}
+	}
+
+}
