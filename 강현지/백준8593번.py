9ab9a4fbacd6094af3e0e379bc153945a3e03386
hyeond401 <100653281+hyeond401@users.noreply.github.com>
2022-03-21 09:29:04 +0900

    20220321(1)

---
+++ 강현지/백준8593번.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 09:08:51 2022
+
+@author: guswl
+"""
+
+n = int(input())
+i=1
+sum=0
+for i in range(1,n+1):
+    sum +=i
+    i += 1
+print(sum)
+    
