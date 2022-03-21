c9cf81ecc816fc7aa5c3a9ea32ea6e3d66e5e020
dew02 <syhan0108@naver.com>
2022-03-21 15:27:59 +0900

    Q2742

---
+++ 한이슬/Q_2742.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Q_2742 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = 0;
+		n = sc.nextInt();
+		for (int i=n; i>=1; i--) {
+			System.out.println(i);
+		}
+	}
+
+}
