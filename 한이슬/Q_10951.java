281ad6ac65a4f4a1dd15acbce0fd849daef633cf
dew02 <syhan0108@naver.com>
2022-03-25 10:04:27 +0900

    Q10951

---
+++ 한이슬/Q_10951.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Q_10951 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a=0;
+		int b=0;
+		while(sc.hasNext()) {
+			a=sc.nextInt();
+			b=sc.nextInt();
+			System.out.println(a+b);
+		}
+	}
+
+}
