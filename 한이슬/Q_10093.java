904525af137408960bc84946741f3c8c65b68276
dew02 <syhan0108@naver.com>
2022-04-12 22:19:34 +0900

    Q10093

---
+++ 한이슬/Q_10093.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Q_10093 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(b-a-1);
+		for (int i=a+1; i<b; i++) {
+			System.out.printf("%d ",i);
+		}
+	}
+
+}
