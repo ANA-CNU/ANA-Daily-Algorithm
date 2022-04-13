912cb473f6fe903d33ba8af9b916097d5f7eeb75
dew02 <syhan0108@naver.com>
2022-04-13 22:41:09 +0900

    Q14918

--- 한이슬/Q_14918.java
+++ 한이슬/Q_14918.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 
 public class Q_14918 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		long b=sc.nextLong();
+		System.out.println(a+b);
 	}
 
 }
