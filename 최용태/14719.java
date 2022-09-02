7f757bedcc1ad6579b217e654491308caeac2be8
0-tae <100774819+0-tae@users.noreply.github.com>
2022-09-02 20:23:48 +0900

    Update 14719.java

--- 최용태/14719.java
+++ 최용태/14719.java
@@ -1,142 +1,47 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 
 public class Main {
-    static int N = 0;
-    static int MC = 0;
-    static int[][] Z048 = new int[N][N];
-    static int[][] pv = new int[N][N];
-    public static void sob0(int[][] Z048) {
-        int start = 0;
-        for (int j = 0; j < N; j++) {
-            for (int i = 0; i < N; i++) {
-                boolean isZero=false;
-                start = i;
-                while (start + 1 < N && Z048[start + 1][j] == 0) start++;
-                if (start + 1 < N && (Z048[start + 1][j] == Z048[i][j] || Z048[i][j]==0)) {
-                    if(Z048[i][j]==0) isZero=true;
-                    Z048[i][j] += Z048[start + 1][j];
-                    Z048[start + 1][j] = 0;
-                }
-                if(isZero) i--;
-            }
-        }
-    }
-
-    public static void sob1(int[][] Z048) {
-        int start = 0;
-        for (int j = 0; j < N; j++) {
-            for (int i = N - 1; i >= 0; i--) {
-                boolean isZero=false;
-                start = i;
-                while (start - 1 >=0 && Z048[start - 1][j] == 0) start--;
-                if (start - 1 >=0 && (Z048[start - 1][j] == Z048[i][j] || Z048[i][j]==0)) {
-                    if(Z048[i][j]==0) isZero=true;
-                    Z048[i][j] += Z048[start - 1][j];
-                    Z048[start - 1][j] = 0;
-                }
-                if(isZero) i++;
-            }
-
-        }
-    }
-
-    public static void sob2(int[][] Z048) {
-        int start = 0;
-        for (int j = 0; j < N; j++) {
-            for (int i = 0; i < N; i++) {
-                boolean isZero=false;
-                start = i;
-                while (start + 1 < N && Z048[j][start + 1] == 0) start++;
-                if (start + 1 < N && (Z048[j][start+1] == Z048[j][i]|| Z048[j][i]==0)) {
-                    if(Z048[j][i]==0) isZero=true;
-                    Z048[j][i] += Z048[j][start + 1];
-                    Z048[j][start + 1] = 0;
-                }
-                if(isZero) i--;
-            }
-        }
-    }
-
-    public static void sob3(int[][] Z048) {
-        int start = 0;
-        for (int j = 0; j < N; j++) {
-            for (int i = N - 1; i >= 0; i--) {
-                boolean isZero=false;
-                start = i;
-                while (start - 1 >= 0 && Z048[j][start - 1] == 0) start--;
-                if (start - 1 >= 0 && (Z048[j][start - 1] == Z048[j][i]|| Z048[j][i]==0)) {
-                    if(Z048[j][i]==0) isZero=true;
-                    Z048[j][i] += Z048[j][start - 1];
-                    Z048[j][start - 1] = 0;
-                }
-                if(isZero) i++;
-            }
-        }
-    }
-
-    public static void search(int n, int visited[]) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                Z048[i][j] = a[i][j];
-                pv[i][j]=a[i][j];
-            }
-        }
-        if (n == 5) {
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    MC = Math.max(MC, Z048[i][j]);
-                }
-            }
-            return;
-        } else {
-            for (int T = 0; T < 4; T++) {
-                switch (T) {
-                    case 0:
-                        sob0(Z048); // 상
-                        break;
-                    case 1:
-                        sob1(Z048); // 하
-                        break;
-                    case 2:
-                        sob2(Z048); // 좌
-                        break;
-                    case 3:
-                        sob3(Z048); // 우
-                        break;
-                }
-                search(n + 1, Z048);
-
-                for (int i = 0; i < N; i++) {
-                    for (int j = 0; j < N; j++)
-                        Z048[i][j] = pv[i][j];
-                }
-            }
-        }
-    }
-
-    public static void initAndSearch() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        Z048 = new int[N][N];
-        pv= new int[N][N];
-        for (int i = 0; i < N; i++) {
-            StringTokenizer s = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < N; j++) {
-                Z048[i][j] = Integer.parseInt(s.nextToken());
-            }
-        }
-
-        search(0,{0,0,0,0});
-    }
-
-    public static void run() throws IOException {
-        initAndSearch();
-        System.out.println(MC);
-    }
 
     public static void main(String[] args) throws IOException {
-        run();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer s = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer ss = new StringTokenizer(br.readLine(), " ");
+        int H = Integer.parseInt(s.nextToken());
+        int W = Integer.parseInt(s.nextToken());
+        int ans = 0;
+        boolean[][] a = new boolean[H + 1][W];
+
+        for (int i = 0; i < W; i++) {
+            int h = Integer.parseInt(ss.nextToken());
+            for (int j = 0; j <= h; j++) {
+                a[j][i] = true;
+            }
+        }
+
+        for (int i = H; i > 0; i--) {
+            int j = W - 1;
+            int start = -1;
+            int end = -1;
+            while (j >= 0) {
+                while (j >= 0 && !a[i][j]) j--;
+                if (j >= 0) start = j;
+
+                j = start - 1;
+
+                while (j >= 0 && !a[i][j]) j--;
+                if (j >= 0) end = j;
+
+                if (j<0) {
+                    break;
+                } else{
+                    ans += start - end-1;
+                    j=end;
+                }
+            }
+        }
+
+        System.out.println(ans);
     }
 }
