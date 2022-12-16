401603a6e03549f7fa110713c822ce32ceb6a266
0-tae <100774819+0-tae@users.noreply.github.com>
2022-12-16 11:12:06 +0900

    2502(dp)

---
+++ 최용태/2502(dp).java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    }
+}
