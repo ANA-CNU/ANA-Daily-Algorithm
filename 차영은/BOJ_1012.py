9857663c29f906afc5b0615db85e3f8241f27a3e
YE <98088296+cye-dev@users.noreply.github.com>
2023-01-06 00:01:31 +0900

    Update BOJ_1012.py

--- 차영은/BOJ_1012.py
+++ 차영은/BOJ_1012.py
@@ -1,33 +1,41 @@
-from collections import deque
 import sys
 
-
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
-
-    while queue:
-        x, y = queue.popleft()
-
-        for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-
-            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-
-    return graph[N - 1][M - 1]
+sys.setrecursionlimit(10000)
 
 
-N, M = map(int, sys.stdin.readline().split())
+def dfs(x, y):
+    graph[x][y] = -1
+
+    for i in range(4):
+        nx = dx[i] + x
+        ny = dy[i] + y
+
+        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
+            graph[nx][ny] = -1
+            dfs(nx, ny)
+
+
+T = int(sys.stdin.readline())
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-graph = [
-    list(map(int, sys.stdin.readline().strip()))
-    for _ in range(N)
-]
+for _ in range(T):
+    m, n, k = map(int, sys.stdin.readline().split())
+    graph = [
+        [0] * m
+        for _ in range(n)
+    ]
+    ans = 0
 
-print(bfs(0, 0))
+    for _ in range(k):
+        x, y = map(int, sys.stdin.readline().split())
+        graph[y][x] = 1
+
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                dfs(i, j)
+                ans += 1
+
+    print(ans)
