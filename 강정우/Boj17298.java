fb5be17f628a952055006fda0d12e06d6f552bcb
Kang Jeongwoo <127028334+jungwoo1208@users.noreply.github.com>
2023-07-26 22:28:18 +0900

    오큰수

---
+++ 강정우/Boj17298.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Boj17298 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                answer[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            answer[stack.pop()] = -1;
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < n; i++) {
+            bw.write(answer[i] + " ");
+        }
+        bw.flush();
+    }
+}
