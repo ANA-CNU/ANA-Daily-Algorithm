7b5cc955318a94d0ec384f358b29cf5d04fda721
sion-k <ygonepiece@naver.com>
2023-10-05 23:27:30 +0900

    ANABADA 추첨 웹훅 추가

---
+++ .github/workflows/anabada/anabada.js
@@ -0,0 +1,154 @@
+import fetch from "node-fetch";
+import seedrandom from "seedrandom";
+
+export const getMonthlyCommits = async () => {
+  const repoOwner = "ANA-CNU";
+  const repoName = "ANA-Daily-Algorithm";
+  const baseApiUrl = "https://api.github.com";
+  const perPage = 100; // Maximum per_page value
+  let page = 1;
+
+  // Determine the current year and month in UTC+9 (Japan Standard Time)
+  const currentDateInJST = new Date(
+    new Date().toLocaleString("en-US", { timeZone: "Asia/Tokyo" })
+  );
+  const currentYear = currentDateInJST.getFullYear();
+  const currentMonth = currentDateInJST.getMonth() + 1; // Months are 0-indexed
+  const maximumRequest = 20;
+  let requestCount = 0;
+
+  const monthlyCommits = [];
+
+  while (requestCount < maximumRequest) {
+    // Build the URL to list commits for the current month on the current page
+    const commitsUrl = `${baseApiUrl}/repos/${repoOwner}/${repoName}/commits?since=${currentYear}-${currentMonth
+      .toString()
+      .padStart(2, "0")}-01T00:00:00Z&page=${page}&per_page=${perPage}`;
+
+    // Fetch commits for the current page
+    const response = await fetch(commitsUrl);
+    requestCount++;
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch commits");
+    }
+
+    const commits = await response.json();
+    monthlyCommits.push(...commits);
+
+    // If there are fewer commits than perPage, it means we've reached the last page
+    if (commits.length < perPage) {
+      break;
+    }
+    // Increment the page number for the next request
+    page++;
+  }
+
+  return monthlyCommits;
+};
+
+const filterCommitsByAuthor = (monthlyCommits) => {
+  const commitDateByAuthor = {};
+  const filteredCommits = [];
+
+  monthlyCommits.forEach((commit) => {
+    const name = commit.commit.author.name;
+
+    const commitDateUTC = new Date(commit.commit.author.date);
+    commitDateUTC.setHours(commitDateUTC.getHours() + 9);
+
+    // Extract the date (YYYY-MM-DD) portion
+    const commitDateStr = commitDateUTC.toISOString().split("T")[0];
+
+    if (!commitDateByAuthor[name]) {
+      commitDateByAuthor[name] = new Set();
+    }
+
+    if (!commitDateByAuthor[name].has(commitDateStr)) {
+      commitDateByAuthor[name].add(commitDateStr);
+      filteredCommits.push(commit);
+    }
+  });
+
+  return filteredCommits;
+};
+
+export const getSolveCountByUser = (monthlyCommits) => {
+  monthlyCommits = filterCommitsByAuthor(monthlyCommits);
+  const solveCountByUser = {};
+
+  monthlyCommits.forEach((commit) => {
+    const userName = commit.commit.author.name;
+
+    if (solveCountByUser[userName]) {
+      solveCountByUser[userName]++;
+    } else {
+      solveCountByUser[userName] = 1;
+    }
+  });
+
+  return solveCountByUser;
+};
+
+export const getPrizeRank = (solveCountByUser, seed = "ANA") => {
+  const weightedUserNames = [];
+
+  for (const [userName, commitCount] of Object.entries(solveCountByUser)) {
+    for (let i = 0; i < commitCount; i++) {
+      weightedUserNames.push(userName);
+    }
+  }
+
+  const shuffledUserNames = shuffleWithSeed(weightedUserNames, seed);
+  const prizeRank = [...new Set(shuffledUserNames)];
+
+  return prizeRank;
+};
+
+export const getRecentSolved = (monthlyCommits) => {
+  monthlyCommits = filterCommitsByAuthor(monthlyCommits);
+  const recentSolved = [];
+
+  monthlyCommits.forEach((commit) => {
+    const userName = commit.commit.author.name;
+    const commitDate = new Date(commit.commit.author.date);
+
+    recentSolved.push({
+      userName,
+      commitDate,
+    });
+  });
+
+  return recentSolved;
+};
+
+export const filterBlackList = (monthlyCommits) => {
+  const blackList = ["sion-k"];
+  const filteredCommits = [];
+
+  monthlyCommits.forEach((commit) => {
+    const name = commit.commit.author.name;
+
+    if (!blackList.includes(name)) {
+      filteredCommits.push(commit);
+    }
+  });
+
+  return filteredCommits;
+};
+
+function shuffleWithSeed(arr, seed) {
+  const rng = seedrandom(seed);
+  const seedRand = (func, min, max) => {
+    return Math.floor(func() * (max - min + 1)) + min;
+  };
+  const resp = [];
+  const keys = Object.keys(Array.from(new Array(arr.length)));
+  for (let i = 0; i < arr.length; i++) {
+    const r = seedRand(rng, 0, keys.length - 1);
+    const g = keys[r];
+    keys.splice(r, 1);
+    resp.push(arr[Number(g)]);
+  }
+  return resp;
+}
