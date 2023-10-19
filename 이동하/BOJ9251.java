a351c5bbcaa1ec509ea6e0786600070c000ae9e6
ldh0906 <a30308104@gmail.com>
2023-10-19 22:41:02 +0900

    백준9251번 문제

--- 이동하/BOJ9251.java
+++ 이동하/BOJ9251.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main{
+public class BOJ9251{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String one = br.readLine();
