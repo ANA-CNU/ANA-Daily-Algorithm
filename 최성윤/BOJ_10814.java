fe8f01c2b1bfe32234c1daf24173069f9880ade0
SeongyunChoi <80454348+csy100886@users.noreply.github.com>
2024-04-13 01:32:52 +0900

    Create BOJ_10814.java

---
+++ 최성윤/BOJ_10814.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[][] arr = new String[N][2];
+
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = st.nextToken();	
+            arr[i][1] = st.nextToken();	
+        }
+
+
+        Arrays.sort(arr, new Comparator<String[]>() {
+
+            @Override
+            public int compare(String[] s1, String[] s2) {
+                return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+            }
+
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++) {
+            sb.append(arr[i][0]).append(' ').append(arr[i][1]).append('\n');
+        }
+
+        System.out.println(sb);
+
+    }
+}
